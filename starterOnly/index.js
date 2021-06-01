504bd527f1b03ea25aad03989099944d8821cd37
Filoupegase <corentin.loison@omnilog>
2021-06-02 00:00:04 +0200

    feat/form all most finish

--- starterOnly/index.js
+++ starterOnly/index.js
@@ -33,54 +33,112 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
 function validFormFirstName() {
   let preRegExp = new RegExp("^[a-zA-ZÀ-ú-s]+$");
-  let errorPre = "ce champs n'est pas valide";
+  let errorPre = "ce champ n'est pas valide";
+  var border = document.getElementById("prenom");
 
   if (!preRegExp.test(this["prenom"].value)) {
     document.getElementById("errorprenom").textContent = errorPre;
+    border.style.border="2px solid red";
     return false;
   }
   document.getElementById("errorprenom").textContent = "";
+  border.style.border="";
   return true;
 }
-
 
 function validFormLastName() {
   let nomRegExp = new RegExp("^[a-zA-ZÀ-ú-s]+$");
-  let errorNom = "ce champs n'est pas valide";
+  let errorNom = "ce champ n'est pas valide";
+  var border = document.getElementById("nom");
 
   if (!nomRegExp.test(this["nom"].value)) {
     document.getElementById("errornom").textContent = errorNom;
+    border.style.border="2px solid red";
     return false;
   }
   document.getElementById("errornom").textContent = "";
+  border.style.border="";
   return true;
 }
 
-
 function validFormEmail() {
   let mailRegExp = new RegExp("^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$");
-  let erroremail = "ce champs n'est pas valide";
+  let erroremail = "ce champ n'est pas valide";
+  var border = document.getElementById("email");
 
   if (!mailRegExp.test(this["email"].value)) {
     document.getElementById("errormail").textContent = erroremail;
+    border.style.border="2px solid red";
     return false;
   }
   document.getElementById("errormail").textContent = "";
+  border.style.border="";
   return true;
 }
 
 function validBirthDate() {
   let errorbirthdate = "Vous devez entrer votre date de naissance.";
   let valueDate = document.getElementById('birthdate').value;
+  var border = document.getElementById("birthdate");
 
   if (!valueDate) {
     document.getElementById("errorbirthdate").textContent = errorbirthdate;
+    border.style.border="2px solid red";
     return false;
   }
   document.getElementById("errorbirthdate").textContent = "";
+  border.style.border="";
+  return true;
+}
+
+function validQuantity() {
+  let errorquantity = "Vous devez entrer une valeur";
+  const quantityTournament = document.getElementById('quantity');
+  const numbers = /^[0-9]+$/;
+  var border = document.getElementById("quantity");
+
+  if (quantityTournament.value < 0 || quantityTournament.value === "") {
+    document.getElementById("errorquantity").textContent = errorquantity;
+    border.style.border="2px solid red";
+    return false;
+  } else if (!quantityTournament.value.match(numbers)) {
+    document.getElementById("errorquantity").textContent = "Veuillez saisir un nombre";
+    border.style.border="2px solid red";
+    return false;
+  }
+  document.getElementById("errorquantity").textContent = "";
+  border.style.border="";
+  return true;
+}
+
+function validLocCheckbox() {
+  let errorLocation = "Veuillez choisir une ville";
+  const loc1 = document.getElementById('location1');
+  const loc2 = document.getElementById('location2');
+  const loc3 = document.getElementById('location3');
+  const loc4 = document.getElementById('location4');
+  const loc5 = document.getElementById('location5');
+  const loc6 = document.getElementById('location6');
+
+  if (!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) {
+    document.getElementById("errorlocation").textContent = errorLocation;
+    return false;
+  }
+  document.getElementById("errorlocation").textContent = "";
+  return true;
+}
+
+function validCheckbox() {
+  let errorCheckbox = "Vous devez lire et accepter les conditions d'utilisation.";
+  const check1 = document.getElementById('checkbox1');
+
+  if (!check1.checked) {
+    document.getElementById("errorcheckbox").textContent = errorCheckbox;
+    return false;
+  }
+  document.getElementById("errorcheckbox").textContent = "";
   return true;
 }
 
@@ -98,6 +156,15 @@ document.forms["form"].addEventListener("submit", function (e) {
 
   const validBirthDates = validBirthDate();
   console.log('validBirthDates :', validBirthDates);
+
+  const validFromQuantity = validQuantity();
+  console.log('validFromQuantity :', validFromQuantity);
+
+  const validCheckboxLocation = validLocCheckbox();
+  console.log('validCheckboxLocation :', validCheckboxLocation);
+
+  const validCheckboxOne = validCheckbox();
+  console.log('validCheckboxOne :', validCheckboxOne);
 
   const checkValid = this.checkValidity();
   console.log('checkValid :', checkValid)
